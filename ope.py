b7ecca493496f14230188668342b4cb15c3978a8
korneliusztx <50407299+korneliusztx@users.noreply.github.com>
2021-08-14 20:24:45 +0700

    Create ope.py

---
+++ ope.py
@@ -0,0 +1,24 @@
+print(
+"Oprasi Hitung Cornelius
+/n 1.Penjumlahan
+/n 2.Pengurangan
+/n 3.Pperklaian
+/n 4.Pembagian")
+x = int(input("masukan 1: "))
+y = int(input("masukan 2: "))
+a = int(input("operasi hitung: "))
+    
+    if a = 1:
+        z = x + y
+        print(z)
+    elif a = 2:
+        z = x - y
+        print(z)
+    elif a = 3
+        z = x * y:
+        print(z)
+    elif a = 4
+        z = x / y
+        print(z)
+    else : 
+    print ("program error ")
